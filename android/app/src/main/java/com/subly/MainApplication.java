7448f496a27c17edaa53ff7797615456dd984e21
Pradeep Tomer <pradeeptomer@Pradeeps-MacBook-Air.local>
2025-06-04 09:33:42 +0530

    Project setUp

--- android/app/src/main/java/com/subly/MainApplication.java
+++ android/app/src/main/java/com/subly/MainApplication.java
@@ -1,4 +1,4 @@
-package com.assignment;
+package com.subly;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/SublyTests/SublyTests.m
+++ ios/SublyTests/SublyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AssignmentTests : XCTestCase
+@interface SublyTests : XCTestCase
 
 @end
 
-@implementation AssignmentTests
+@implementation SublyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
